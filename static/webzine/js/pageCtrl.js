902f32be5ff3471283e778aeedff54a674e1724a
Puritechlab <pub@puritechlab.com>
2025-07-07 15:39:57 +0900

    Initial commit

---
+++ static/webzine/js/pageCtrl.js
@@ -0,0 +1,11 @@
+
+        // 페이지 로드 시 네비게이션 설정
+        document.addEventListener('DOMContentLoaded', setupNavigation);
+
+        // 맨 위로 스크롤 이동 함수
+        function scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
