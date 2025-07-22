9c246804ef6c0735371c98109ecf6007c5baf6dd
puritechlab1155 <pub@puritechlab.com>
2025-07-22 10:53:24 +0900

    새소식 이미지 원복 및 콘솔에서 수정

--- static/webzine/js/pageCtrl.js
+++ static/webzine/js/pageCtrl.js
@@ -1,6 +1,6 @@
 
         // 페이지 로드 시 네비게이션 설정
-        document.addEventListener('DOMContentLoaded', setupNavigation);
+        // document.addEventListener('DOMContentLoaded', setupNavigation);
 
         // 맨 위로 스크롤 이동 함수
         function scrollToTop() {
